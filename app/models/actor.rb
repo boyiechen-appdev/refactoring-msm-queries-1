830f57cabd97a603708c27562cdae02392246492
boyiechen <boyie@uchicago.edu>
2023-04-25 15:37:20 +0000

    add instance method in actor class

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -11,4 +11,13 @@
 #  updated_at :datetime         not null
 #
 class Actor < ApplicationRecord
+  def characters
+    # <% a_id = @the_actor.id %>
+
+    # <% matching_characters = Character.where({ :actor_id => a_id }) %>
+
+    # <% matching_characters.each do |a_character| %>
+    matching_characters = Character.where({ :actor_id => self.id })
+    return matching_characters
+  end
 end
